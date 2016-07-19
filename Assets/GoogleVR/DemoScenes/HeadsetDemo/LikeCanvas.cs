6af46a308828ce94ccf55405b0c5747410d5bc05
Lekh Pokharel <lpokharel@godaddy.com>
2016-07-19 14:57:04 -0400

    checkin

---
+++ Assets/GoogleVR/DemoScenes/HeadsetDemo/LikeCanvas.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LikeCanvas : MonoBehaviour {
+
+	private ArrayList list = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+		list.Add ("one.com");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//list.Add ("two.com");
+	}
+}
